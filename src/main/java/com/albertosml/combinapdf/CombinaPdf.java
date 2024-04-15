87b8916f8146c4a4ca827da8826304df8ea475d8
Alberto Silvestre Montes Linares <albertosilvestremonteslinares@gmail.com>
2024-04-15 23:41:05 +0200

    Switch to maven in order to update Maven dependencies

---
+++ src/main/java/com/albertosml/combinapdf/CombinaPdf.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.albertosml.combinapdf;
+
+/**
+ *
+ * @author albertosilvestremonteslinares
+ */
+public class CombinaPdf {
+
+    public static void main(String[] args) {
+        // Abrir la pantalla de inicio en modo pantalla completa.
+        Inicio i = new Inicio();
+        i.setVisible(true);
+    }
+}
